aa178ce458e135bd0710037c1a7346f4bab3ad19
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-12-07 19:11:44 +0100

    validation

---
+++ scripts/distance_serviceTime_distributions.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import csv
+
+M = 25000
+k = 1.6e-9 #???
+T = 0.000000001
+
+### Distribution of Distance
+# PDF of distance
+def fd(d):
+    if d >= 0 and d <= M/2:
+        return k*2*np.pi*d
+    elif d > M/2 and d <= M/(2**(1/2)):
+        #return k*(2*np.pi*d - 8*d*np.arccos(M/(2*(2**(1/2))*d)))
+        return k*4*d*(np.pi/2 - 2*np.arccos(M/2/d))
+    else:
+        return 0
+
+d = []
+f = []
+
+# compute fd in our interval of interest
+d = np.arange(0, (M * 2**0.5 / 2))
+for i in d:
+    f.append(fd(i))
+
+# compute integral of fd
+integral = []
+last = 0
+for i in range(0, len(f)):
+    part = f[i]
+    last = last + part
+    integral.append(last)
+
+print("normalization fd: ", np.trapz(f))
+
+# make plots
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('d [m]')
+ax1.set_ylabel('fD(d)')
+ax1.ticklabel_format(axis='y', style='sci', scilimits=(-4,-4))
+ax1.plot(d, f, 'r-')
+
+ax2 = ax1.twinx()
+
+color = 'tab:blue'
+ax2.set_ylabel('FD(d)')
+ax2.plot(d, integral, 'b-')
+
+handles = []
+handles.append(mpatches.Patch(color='red',  label=r'$f_D(d)$'))
+handles.append(mpatches.Patch(color='blue', label=r'$F_D(d)$'))
+plt.legend(handles=handles)
+plt.grid(True, axis='both')
+plt.savefig(fname="plots/plot_distance_distribution")
+plt.figure()
+
+def fs(s):
+    #if s >= 0 and s <= (T * M ) / 2:
+    if s >= 0 and s <= (T * M**2 ) / 4:
+        #return 2*np.pi*s / ( T **2 )
+        return np.pi / ( T * M**2 )
+    #elif s > (T * M ) / 2 and s <= (T * M * (2**0.5) ) / 2:
+    elif s >= (T * M**2 ) / 4 and s <= (T * M**2) / 2:
+        #return (2*np.pi*s / (T**2)) - (8 * s/ (T **2)) * np.arccos((M*T*(2**0.5))/(2*s))
+        return (np.pi / (T * M**2)) - (4 / (T * M**2)) * np.arccos(M/2 * ((T / s)**0.5))
+    else:
+        return 0
+
+### Distribution of Service Time
+#s = T * d
+s = T * d ** 2
+f = []
+for i in s:
+    f.append(fs(i))
+
+print("normalization fs: ", np.trapz(f, s))
+
+# compute integral of fs
+integral = [ 0 ]
+last = 0
+for i in range(1, len(f)):
+    part = f[i] * (s[i] - s[i - 1])
+    last = last + part
+    integral.append(last)
+
+# make plots
+fig, ax1 = plt.subplots()
+color = 'tab:red'
+ax1.set_xlabel('s [s]')
+ax1.set_ylabel('fS(s)')
+ax1.plot(s, f, 'r-')
+
+ax2 = ax1.twinx()
+
+color = 'tab:blue'
+ax2.set_ylabel('FD(d)')
+ax2.plot(s, integral, 'b-')
+
+handles = []
+handles.append(mpatches.Patch(color='red',  label=r'$f_S(s)$'))
+handles.append(mpatches.Patch(color='blue', label=r'$F_S(s)$'))
+plt.legend(handles=handles)
+plt.grid(True, axis='both')
+plt.savefig(fname="plots/plot_service_time_distribution")
+plt.figure()
---
+++ scripts/fittingServingTime.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from scipy.stats import uniform as unif
+from scipy.stats import linregress as regr
+#import seaborn as sns
+import matplotlib
+matplotlib.rcParams['font.family'] = "serif"
+plt.style.use('ggplot')
+
+M = 25000
+T = 0.00000000425603
+nameOrder = ['interarrival', 'serviceTime',
+             'queueLength', 'responseTime', 'waitingTime']
+
+'''
+    Load and rename columnn using nameOrder list.
+    CSV File must containt data with the following order:
+    time, interarrival, time, serviceTime, time, queueLength, time, responseTime, time, waitingTime 
+    repeated {repetition} times 
+
+    columns of time are deleted
+'''
+
+
+def loadData(path, repetition):
+    df = pd.read_csv(path + '.csv')
+    names = []
+    for i in range(repetition):
+        for j in range(len(nameOrder)):
+            names.append('time')
+            names.append(nameOrder[j] + str(i))
+
+    df.columns = names
+    df = df.drop(columns='time')
+    df = df[:math.floor(len(df)*0.7)]
+    return df
+
+
+'''
+    Find every column of dataframe that starts with "stat"
+'''
+
+
+def splitStat(df, stat):
+    statDf = df[df.columns[pd.Series(df.columns).str.startswith(stat)]]
+    return statDf
+
+
+'''
+    Sort every column of dataframe and perform a mean per row
+'''
+
+
+def meanPerRow(df, stat):
+    orderedStat = df.apply(lambda x: x.sort_values().values)
+    orderedStat = orderedStat.apply(lambda x: x.dropna())
+    return pd.DataFrame(orderedStat.mean(axis=1), columns=[stat])
+
+
+def histogram(df, nbin, path, name, k):
+    plt.figure()
+    n, bins, patches = plt.hist(
+        df['Mean_' + name], nbin, density=True, facecolor='g', alpha=0.75)
+    plt.title('Histogram of ' + name + ' ' + k + 's')
+    plt.grid(True)
+    plt.savefig(path + '.png')
+
+
+'''
+    Service Time Cumulative Distribution Function F(s)
+'''
+
+
+def serviceTimeCDF(s):
+    if s < 0:
+        Fs = 0
+    elif s <= T*M**2/4:
+        Fs = math.pi * s/(T*M**2)
+    elif s <= T*M**2/2:
+        Fs = math.pi * s/(T*M**2) - 4*s/(T*M**2)*math.acos(M /
+                                                           2*math.sqrt(T/s)) + math.sqrt(4*s/(T*M**2)-1)
+    else:
+        Fs = 1.0
+    return Fs
+
+
+'''
+    Distance Cumulative Distribution Function F(d)
+'''
+
+
+def distanceCDF(d):
+    if d < 0:
+        Fd = 0
+    elif d <= M/2:
+        Fd = math.pi*d**2/M**2
+    elif d <= M/2*math.sqrt(2):
+        Fd = math.pi*d**2/M**2 - 4/M**2*d**2 * \
+            math.acos(M/(2*d)) + d/M*math.sqrt((4*d**2-M**2)/d**2)
+    else:
+        Fd = 1.0
+    return Fd
+
+
+'''
+    look for x such that F(x) = P{X < x} = quantile, with an error of maxError
+'''
+
+
+def findQuantile(quantile, name, maxError):
+    x = 0.0
+    if name == 'serviceTime':
+        error = quantile - serviceTimeCDF(x)
+        while error > maxError:
+            x += 0.1*error
+            error = quantile - serviceTimeCDF(x)
+    elif name == 'distance':
+        error = quantile - distanceCDF(x)
+        while error > maxError:
+            x += 0.3*error
+            error = quantile - distanceCDF(x)
+    return x
+
+
+'''
+     find every quantile for both theoretical and sample distribution
+'''
+
+
+def fitDistribution(df, name, maxError):
+    theoreticalQ = []
+    sampleQ = []
+    for i in range(1, len(df)):
+        quantile = (i-0.5)/len(df)
+        sq = df[name].quantile(quantile)
+        tq = findQuantile(quantile, name, maxError)
+        sampleQ.append(sq)
+        theoreticalQ.append(tq)
+        print(quantile, tq, sq)
+    return [theoreticalQ, sampleQ]
+
+
+'''
+     draw a qq plot
+'''
+
+
+def qqPlot(theoreticalQ, sampleQ, name):
+    slope, intercept, r_value, p_value, std_err = regr(theoreticalQ, sampleQ)
+
+    plt.figure()
+    plt.scatter(theoreticalQ, sampleQ, s=0.8, label=name, c='blue')
+    y = [x*slope + intercept for x in theoreticalQ]
+    plt.plot(theoreticalQ, y, 'r', label='Trend line')
+    plt.text(0, max(sampleQ)*0.6, '\n\n$R^2$ = ' + str('%.6f' % r_value**2))
+    if intercept > 0:
+        plt.text(0, max(sampleQ)*0.55, 'y = ' + str('%.6f' %
+                                                    slope) + 'x + ' + str('%.6f' % intercept))
+    else:
+        plt.text(0, max(sampleQ)*0.55, 'y = ' + str('%.6f' %
+                                                    slope) + 'x ' + str('%.6f' % intercept))
+
+    plt.xlabel('Theoretical Quantile')
+    plt.ylabel('Sample Quantile')
+    plt.title('QQ plot ' + name)
+    plt.grid(True)
+    plt.legend()
+
+
+def main():
+    '''
+        Service Time fitting
+    '''
+    # data = loadData('K2s', 30)
+    # serviceTime30Rep = splitStat(data, 'serviceTime')
+    # serviceTime = meanPerRow(serviceTime30Rep, 'serviceTime')
+
+    # serviceTime = serviceTime.sample(n=1000)
+
+    # theoreticalQ, sampleQ = fitDistribution(serviceTime, 'serviceTime', 0.0001)
+    # qqPlot(theoreticalQ, sampleQ, 'serviceTime' )
+
+    # plt.savefig(fname="plots/distance_qq_plot")
+
+    '''
+        Distance Time fitting
+    # '''
+    distance30Rep = pd.read_csv('distance.csv')
+    names = []
+    for i in range(30):
+        names.append('time')
+        names.append('distance' + str(i))
+
+    distance30Rep.columns = names
+    distance30Rep = distance30Rep.drop(columns='time')
+    distance = meanPerRow(distance30Rep, 'distance')
+    distance = distance.sample(n=1000)
+
+    theoreticalQ, sampleQ = fitDistribution(distance, 'distance', 0.0001)
+    qqPlot(theoreticalQ, sampleQ, 'distance')
+
+    plt.savefig(fname="plots/distance_qq_plot")
+
+
+if __name__ == "__main__":
+    main()
